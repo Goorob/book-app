acf75b7f00b243048b3beccb146ff10e0113b6d7
Goorob <orobalswalqah@gmail.com>
2019-11-26 11:05:39 +0200

    render

--- server.js
+++ server.js
@@ -12,12 +12,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
+app.get('/searches', (req, res) => {
     res.render('pages/index');
 });
 
 
-
+// app.get('/show', (req, res) => {
+//     res.render('pages/searches/show' ,{books:req.books});
+// });
 
 
 
@@ -44,28 +46,29 @@ app.get('/', (req, res) => {
 //         })}
 
 
-function Book(data) {
-    this.title = data.volumeInfo.title;
-    this.author = data.volumeInfo.authors[0]  ;
-    this.image_url = data.volumeInfo.imageLinks.thumbnail ;
-    this.description = data.volumeInfo.description  ;
-    this.type= data.volumeInfo.industryIdentifiers.type;
-}
+// function Book(data) {
+//     this.title = data.volumeInfo.title;
+//     this.author = data.volumeInfo.authors[0]  ;
+//     this.image_url = data.volumeInfo.imageLinks.thumbnail ;
+//     this.description = data.volumeInfo.description  ;
+//     this.type= data.volumeInfo.industryIdentifiers.type;
+// }
 
-app.get('/searches', (req, res) => {
-    let url = 'https://www.googleapis.com/books/v1/volumes?q=dan';
+app.post('/searches', (req, res) => {
+    let url = 'https://www.googleapis.com/books/v1/volumes?q=Dan Brown';
 
     superagent.get(url)
         .then(data => {
             // res.json(data.body)
             // console.log(data);
-            let books=data.body ;
-            return books.items.map(book=>{
-                return new Book(book);
-            })
+            let books=data.body.items ;
+            res.render('pages/show', {books:books})
+            // return books.items.map(book=>{
+            //     // return new Book(book);
+            //     res.render('/show' , {books:book})
+            // })
             
         });
-        res.render('pages/searches/show' , {books:book})
 });
 
 
